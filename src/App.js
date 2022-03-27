0e84381e7cb7e4d7c5093a72ab0e4a60a99537eb
Diamonique Danner <danner@danner.tech>
2022-03-27 15:20:39 -0400

    added context to App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {createContext} from 'react';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Yellow from './components/Yellow';
 import Green from './components/Green';
 import Blue from './components/Blue';
 
+//default value is lighht
+
+const AppContext = createContext('light');
+
+//we set the actual value to dark when we pass in another value
 const App = () => {
   return(
+    <AppContext.Provider value={'dark'}>
     <BrowserRouter>
     <Routes>
-      
+      <Route path="/yellow" component={<Yellow />}/>
+      <Route path="/blue" component={<Blue />} />
+      <Route path="/green" component={<Green />} />
     </Routes>
     </BrowserRouter>
+    </AppContext.Provider>
   )
 }
 
